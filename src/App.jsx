49313116177120eb3b37cbd59a98d0aab7030a94
Mary Jonah <jonahmary17@gmail.com>
2023-11-29 17:25:47 -0800

    Created LandingPage

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import LandingPage from "./components/LandingPage";
+import "./App.css";
+
+export default function App() {
+  const [firstLoad, setFirstLoad] = React.useState(true);
+  const [allQuestions, setAllQuestions] = React.useState([]);
+  console.log(allQuestions);
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple")
+        .then(res => res.json())
+        .then(data => setAllQuestions(data.results))
+  }, [])
+    
+    return (
+        <main>
+            {
+                firstLoad ? 
+                <LandingPage /> : 
+                <h4>First Page</h4>
+            }
+        </main>
+    )
+}
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LandingPage.css";
+
+export default function LandingPage() {
+    return (
+        <header>
+            <h2>Quizzical</h2>
+            <p>Some description if needed</p>
+            <button>Start quiz</button>
+        </header>
+    )
+}
